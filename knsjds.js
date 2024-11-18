ae29430cb9d39c6ec1b344b064586d0a6e09aaa6
rihsiboy01 <auzcom78@gmail.com>
2024-11-18 16:28:04 +0300

    Add files via upload

---
+++ knsjds.js
@@ -0,0 +1,40 @@
+// №1
+// let a = alert(5)
+// №2
+// let a = Math.round(Math.random()*10)
+// let b = alert(`random son ${a} \n 2 kopaytirilgani ${a*2}`)
+// №3
+// let b = 12.510;
+// console.log(Math.trunc(b)); 
+// №4
+// let a = prompt("yosh kriting")
+// if(11 < a){
+//     console.log('siz katasiz');
+    
+// }else{
+//     console.log('siz chumosiz');
+// }
+// №5
+// let l = document.querySelector('h2');  
+// let s = document.querySelector('h3');  
+
+
+// let b = prompt('Ismingizni kiriting');        
+// let g = prompt('Familiyangizni kiriting');    
+// let d = prompt('Yoshingizni kiriting');       
+
+// a.innerHTML = b;  
+// l.innerHTML = g;  
+// s.innerHTML = d; 
+// №6
+// let data = [1,2,3,4,5,6,7,8,9,]
+// let d = []
+// let g = []
+
+// for (let i = 0; i < data.length; i++) {
+//      if  
+// }
+// №7
+
+
+
